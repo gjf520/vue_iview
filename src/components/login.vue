bdac88e556d8a77ce5e0097ad4fd26f95312a527
gjf520 <1093830589@qq.com>
2018-05-11 15:26:43 +0800

    文件

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <i-button>Default</i-button>
+        <i-button type="primary">Primary</i-button>
+        <i-button type="ghost">Ghost</i-button>
+        <i-button type="dashed">Dashed</i-button>
+        <i-button type="text">Text</i-button>
+        <br><br>
+        <i-button type="info">信息按钮</i-button>
+        <i-button type="success">成功按钮</i-button>
+        <i-button type="warning">警告按钮</i-button>
+        <i-button type="error">错误按钮</i-button>
+    </div>
+</template>
+<script>
+export default {
+        
+    }
+</script>
